2564d894dbba436997b5d26e54ffbfced6060487
Adrian Cuellar <aacuellar14@gmail.com>
2017-11-16 14:21:11 -0800

    Changed a lot

--- src/TicTacToeClient.java
+++ src/TicTacToeClient.java
@@ -8,73 +8,41 @@ public class TicTacToeClient {
     public static void main(String[] args) throws Exception {
         try (Socket socket = new Socket("18.221.102.182",38006)) {
             System.out.println("Connected to server.");
-            InputStream is = socket.getInputStream();
-            PrintStream out = new PrintStream((socket.getOutputStream()),true,"UTF-8");
-
-            ByteArrayOutputStream bO = new ByteArrayOutputStream();
-            ObjectOutputStream oO = new ObjectOutputStream(bO);
-            //BoardMessage Size is 32x based on status
-            //ERROR MESSAGE SIZE is ~175 + String size of error
-            oO.writeObject(new ConnectMessage("Player"));
-            out.write(bO.toByteArray());
-            byte[] temp = new byte[340];
-            is.read(temp);
-            ByteArrayInputStream bI = new ByteArrayInputStream(temp);
-            ObjectInputStream oI = new ObjectInputStream(bI);
-            BoardMessage msg = (BoardMessage) oI.readObject();
+            ObjectOutputStream sendMessage = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream receiveMessage = new ObjectInputStream(socket.getInputStream());
+            sendMessage.writeObject(new ConnectMessage("Player"));
+            sendMessage.writeObject(new CommandMessage(CommandMessage.Command.NEW_GAME));
+            Message msg =(Message) receiveMessage.readObject();
+            if(msg instanceof ErrorMessage) {
+                System.out.println("Error Detected!");
+                socket.close();
+                System.out.println("Disconnected from server.");
+                return;
+            }
             Scanner scanner = new Scanner(System.in);
 
-            //ERROR MESSAGE DETECTION
-            Runnable listen = () ->{
-                while(true){
-                    try{
-                        byte[] tempE= new byte[200];
-                        is.read(tempE);
-                        ByteArrayInputStream bIE = new ByteArrayInputStream(tempE);
-                        ObjectInputStream oIE = new ObjectInputStream(bIE);
-                        ErrorMessage error = (ErrorMessage) oIE.readObject();
-                        System.out.println(error.getError());
-                    }
-                    catch (Exception e){
-
-                    }
-                }
-            };
-
-            Thread listeningThread = new Thread(listen);
-            listeningThread.start();
-
-            while(msg.getStatus().equals(BoardMessage.Status.IN_PROGRESS)){
-                printBoard(msg);
+            while(((BoardMessage) msg).getStatus().equals(BoardMessage.Status.IN_PROGRESS)){
+                printBoard((BoardMessage) msg);
                 System.out.println("Please choose the number corresponding to the action you would like to take:");
                 System.out.println("1. Continue the game, 2. Surrender, 3. Exit, or 4. Start a New Game");
                 System.out.println("Entering a number that isn't one of the above choices will default to continuing the game.");
-                int choice=0;
+                int choice;
                 while(true) {
                     try {
                         choice = scanner.nextInt();
+                        break;
                     } catch (InputMismatchException e) {
                         System.out.println("Please enter one of the integers 1,2,3,4 corresponding to your choice.");
                     }
-                    break;
                 }
                 if(choice==2){
-                    ByteArrayOutputStream bOCM = new ByteArrayOutputStream();
-                    ObjectOutputStream oOCM = new ObjectOutputStream(bOCM);
-                    oOCM.writeObject(new CommandMessage(CommandMessage.Command.SURRENDER));
-                    out.write(bOCM.toByteArray());
+                    sendMessage.writeObject(new CommandMessage(CommandMessage.Command.SURRENDER));
                 }
                 else if(choice==3){
-                    ByteArrayOutputStream bOCM = new ByteArrayOutputStream();
-                    ObjectOutputStream oOCM = new ObjectOutputStream(bOCM);
-                    oOCM.writeObject(new CommandMessage(CommandMessage.Command.EXIT));
-                    out.write(bOCM.toByteArray());
+                    sendMessage.writeObject(new CommandMessage(CommandMessage.Command.EXIT));
                 }
                 else if(choice==4){
-                    ByteArrayOutputStream bOCM = new ByteArrayOutputStream();
-                    ObjectOutputStream oOCM = new ObjectOutputStream(bOCM);
-                    oOCM.writeObject(new CommandMessage(CommandMessage.Command.NEW_GAME));
-                    out.write(bOCM.toByteArray());
+                    sendMessage.writeObject(new CommandMessage(CommandMessage.Command.NEW_GAME));
                 }
                 byte r;
                 byte c;
@@ -91,23 +59,27 @@ public class TicTacToeClient {
                         System.out.println("Nonexistent row, please enter another col.");
                         c = getCol(scanner);
                     }
-                    if(msg.getBoard()[r][c]!=0){
+                    if(((BoardMessage) msg).getBoard()[r][c]!=0){
                         System.out.println("Square designated by row and column is already filled, please try again.");
                     }
                     else{
                         break;
                     }
                 }
-                ByteArrayOutputStream bOM = new ByteArrayOutputStream();
-                ObjectOutputStream oOM = new ObjectOutputStream(bOM);
-                oOM.writeObject(new MoveMessage(r,c));
-                out.write(bOM.toByteArray());
+                sendMessage.writeObject(new MoveMessage(r,c));
+                msg = (Message) receiveMessage.readObject();
+                if(msg instanceof ErrorMessage) {
+                    System.out.println("Error Detected!");
+                    socket.close();
+                    System.out.println("Disconnected from server.");
+                    return;
+                }
             }
-            System.out.println(msg.getStatus());
-            printBoard(msg);
-            is.close();
+            System.out.println(((BoardMessage) msg).getStatus());
+            printBoard(((BoardMessage) msg));
             socket.close();
-            System.out.println("Disconnected from server.");
+            System.out.println("The game is over.");
+            System.out.println(" Disconnected from server.");
         }
     }
 
@@ -129,26 +101,26 @@ public class TicTacToeClient {
     }
 
     public static byte getRow(Scanner scanner){
-        byte r=0;
+        byte r;
         while(true) {
             try {
                 r = scanner.nextByte();
+                return r;
             } catch (InputMismatchException e) {
                 System.out.println("Please enter one of the one of the bytes 0,1,2 corresponding to your choice of row.");
             }
-            return r;
         }
     }
 
     public static byte getCol(Scanner scanner){
-        byte c=0;
+        byte c;
         while(true) {
             try {
                 c = scanner.nextByte();
+                return c;
             } catch (InputMismatchException e) {
                 System.out.println("Please enter one of the one of the bytes 0,1,2 corresponding to your choice of col.");
             }
-            return c;
         }
     }
 }
